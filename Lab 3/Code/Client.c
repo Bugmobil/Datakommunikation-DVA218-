b191513219b7ffc4c9464d950c50cae70c29024b
Andreas <apn21017@student.mdu.se>
2023-04-25 16:49:02 +0200

    Testing

---
+++ Lab 3/Code/Client.c
@@ -0,0 +1,6 @@
+#include "Protocol.h"
+
+int main(int argc, char *argv[])
+{
+    printf("Hello There!\n");
+}
--- Lab 3/Code/Protocol.h
+++ Lab 3/Code/Protocol.h
@@ -1,3 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <stdint.h>
+
+
 struct protocol
 {
     int source;
