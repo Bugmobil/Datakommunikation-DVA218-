124515e8d00254efff56b7c1420f65bb650df057
Bugmobil <76096331+Bugmobil@users.noreply.github.com>
2023-05-05 16:22:03 +0200

    File descriptions

---
+++ Lab 3/Code/Server.c
@@ -0,0 +1,4 @@
+/*
+This file contains the implementation of the functions declared in server.h. 
+It should handle the server-side logic for listening for connections, receiving data, and sending acknowledgments.
+*/
