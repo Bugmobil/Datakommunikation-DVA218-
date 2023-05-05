83d3b4b4688a7627dab892f66724304ad7013810
Bugmobil <majid.azizi@hotmail.se>
2023-05-05 16:52:13 +0200

    mervge

--- Lab 3/Code/Client.c
+++ Lab 3/Code/Client.c
@@ -1,23 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#include <time.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <errno.h>
-#include <stdint.h>
-#include <netdb.h>
+#include <Utils.h>
 
 
 int main(int argc, char *argv[])
 {
     int sockfd;
     struct sockaddr_in server_addr, client_addr;
+    char hostName[hostNameLength];
     char buffer[] = "Hello from client";
+
+    if(argv[1] == NULL)
+    {
+        perror("Usage: client [host name]\n");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        strncpy(hostName, argv[1], hostNameLength);
+        hostName[hostNameLength - 1] = '\0';
+    }
 
     // Create a UDP socket
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -34,30 +34,5 @@ int main(int argc, char *argv[])
     close(sockfd);
 
     return 0;
-    
-/*
-This file contains the implementation of the functions declared in client.h. 
-It should handle the client-side logic for connecting to a server, sending data, and receiving acknowledgments.
-*/
 
-#include "Utils.h"
-
-int main(int argc, char *argv[])
-{
-    int sock;
-    char hostName[hostNameLength];
-    Packet packetToSend;
-
-    printf("Fueled up, ready to go!\n");
-
-    if(argv[1] == NULL)
-    {
-        perror("Usage: client [host name]\n");
-        exit(EXIT_FAILURE);
-    }
-    else
-    {
-        strncpy(hostName, argv[1], hostNameLength);
-        hostName[hostNameLength - 1] = '\0';
-    }
 }
